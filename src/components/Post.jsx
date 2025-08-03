095a35cfac7a980b45ad41782805c3e7d50e2cfa
Franciane SL <fran.schier@outlook.com>
2025-08-03 14:35:55 -0300

    Atualização do componente Post.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,33 +1,16 @@
-
-import { useEffect, useState } from 'react';
 import style from './Post.module.css';
 
-function Post() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const dadosSalvos = localStorage.getItem("posts");
-    if (dadosSalvos) {
-      const dados = JSON.parse(dadosSalvos);
-      console.log("DADOS CARREGADOS:", dados);
-      setPosts(dados);
-    }
-  }, []);
-
+function Post({ tipo, titulo, descricao, data, url }) {
   return (
-    <>
-      {posts.map((post, index) => (
-        <section className={style.sectionPost} key={index}>
-          <img src={post.url} alt={`Imagem do post ${post.titulo}`} />
-          <div className={style.cardContent}>
-            <span>{post.type}</span>
-            <h2>{post.titulo}</h2>
-            <p>{post.descricao}</p>
-            <p><strong>Data de publicação:</strong> {post.data}</p>
-          </div>
-        </section>
-      ))}
-    </>
+    <section className={style.sectionPost}>
+      <img src={url} alt={`Imagem do post ${titulo}`} />
+      <div className={style.cardContent}>
+        <span>{tipo}</span>
+        <h2>{titulo}</h2>
+        <p>{descricao}</p>
+        <p><strong>Data de publicação:</strong> {data}</p>
+      </div>
+    </section>
   );
 }
 
